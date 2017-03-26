14496fd62155670f01c96390bc36847107b8ce7f
shijingyu <945090896@qq.com>
2017-03-27 00:53:35 +0800

    online test

--- MkStone/settings.py
+++ MkStone/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'MkStone',
         'USER': 'root',
-        'PASSWORD': '960722',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
